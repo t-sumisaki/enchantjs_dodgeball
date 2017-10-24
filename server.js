e840f302cbe56ea3de9f0306eb7d158d02a9549c
T_Sumisaki <mail.sumisaki@gmail.com>
2017-10-25 01:00:30 +0900

    create data-sync prototype

--- server.js
+++ server.js
@@ -2,10 +2,48 @@ const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const path = require('path')
-
+const io = require('socket.io')(http);
 
 
 app.use(express.static(path.resolve(__dirname, 'webroot')));
+
+let id = 0;
+let players = []
+
+io.on('connection', (socket) => {
+
+    let _id = ++id;
+    console.log('connected.', _id);
+
+    socket.on('login', () => {
+        console.log('login', _id);
+        socket.emit('login', {
+            id: _id
+        })
+    })
+    
+    socket.on('checkuser', function() {
+        console.log('receive: checkuser', players);
+        socket.emit('checkuser', players);
+    })
+
+    players.push(_id)
+
+    socket.on('disconnect', function() {
+        console.log('disconnect:', _id);
+
+        let index = players.indexOf(_id);
+        if (index > -1) {
+            players.splice(index, 1);
+        }
+
+        socket.broadcast.emit('disconnected');
+    })
+
+    socket.on('sync', function(data) {
+        socket.broadcast.emit('sync', data)
+    })
+})
 
 
 http.listen(3000, () => {
--- webroot/js/main.js
+++ webroot/js/main.js
@@ -2,6 +2,16 @@
 
     enchant();
 
+    const socket = io.connect('/');
+
+
+    // 接続ID。サーバから発行する
+    let myId = null;
+
+
+
+
+
 
     const game = new Game(320, 320);
 
@@ -12,35 +22,146 @@
     // game.keybind('Z'.charCodeAt(0), 'shot');
     game.keybind(' '.charCodeAt(0), 'shot')
 
+
+
     game.onload = function () {
         game.rootScene.backgroundColor = '#7ecef4';
-        let player = new Player(1, 200, 200);
+
+        let label = new Label("RootScene");
+        game.rootScene.addChild(label);
+
+        const connectScene = new ConnectScene(game, socket);
+        console.log('initialize connectscene')
+        game.rootScene.addEventListener('shotbuttondown', function () {
+            socket.emit('login');            
+            //game.pushScene(connectScene);
+        })
+
+        socket.on('login', function(data) {
+            game.myId = data.id;
+            game.pushScene(connectScene);
+        })
+
+        const gameScene = new GameScene(game, socket);
+        game.connected = function(_players) {
+            console.log('connected');
+            gameScene.restart(game.myId, _players);
+            game.replaceScene(gameScene)
+        }
+
+        socket.on('disconnected', function() {
+            console.log('disconnected');
+            game.disconnected();
+        })
+
+        game.disconnected = function() {
+            game.popScene();
+        }
+
+        /*
+        let player = new Player(myId, 100, 100);
+        player.move = new MoveCommand(player);
+        players.push(player)
         game.rootScene.addChild(player);
 
         game.rootScene.addEventListener('enterframe', function () {
             player.move(game);
-            player.animation();
         })
 
-        game.rootScene.addEventListener('shotbuttondown', function() {
+        game.rootScene.addEventListener('shotbuttondown', function () {
             player.shot();
         })
 
         let projectiles = []
         for (let i = 0; i < 1; ++i) {
-            let _projectile = new Projectile(player.id);
+            let _projectile = new Projectile(player.playerId);
             game.rootScene.addChild(_projectile);
             projectiles.push(_projectile);
         }
 
         player.projectiles = projectiles;
 
+        let other = new Player(2, 200, 200);
+        game.rootScene.addChild(other);
+        players.push(other);
+        */
+
 
     }
-
     game.start();
 
+    socket.on('login', function (data) {
+        myId = data.id;
+    })
 
+
+    const ConnectScene = Class.create(Scene, {
+        initialize: function (_game, _socket) {
+            Scene.call(this);
+            // 接続用・接続待ちシーン
+            this.interval = 24;
+            this.count = 0;
+
+            _socket.on('checkuser', function (data) {
+                console.log('receive: checkuser');
+                if (data.length >= 2) {
+                    _game.connected(data);
+                }
+            })
+
+            this.addEventListener('enterframe', function () {
+                if (this.count < this.interval) {
+                    ++this.count;
+                    return;
+                } else {
+                    _socket.emit('checkuser');
+                    this.count = 0;
+                }
+            })
+        }
+    })
+
+    const GameScene = Class.create(Scene, {
+        initialize: function (_game, _socket) {
+            Scene.call(this);
+
+            let players = new Group();
+            this.addChild(players);
+
+            this.player = null;
+
+            this.restart = function (_myid, _players) {
+
+                while(players.childNodes.length > 0) {
+                    players.removeChild(players.childNodes[0])
+                }
+
+                for (let i = 0, _p; _p = _players[i]; ++i) {
+                    let player = new Player(_p, 100 * (i + 1), 100);
+
+                    if (_p === _myid) {
+                        player.move = new MoveCommand(player, _socket);
+                        this.player = player;
+                    }
+
+                    players.addChild(player);
+                }
+            }
+
+            _socket.on('sync', function(data) {
+                for(let _node of players.childNodes) {
+                    if (_node.playerId == data.playerId) {
+                        _node.sync(data);
+                    }
+                }
+            })
+
+
+            this.addEventListener('enterframe', function() {
+                this.player.move(_game);                
+            })
+        }
+    })
 
 
 
@@ -52,16 +173,35 @@
             this.x = _x;
             this.y = _y;
             this.frame = 0;
-            this.id = _id;
+            this.playerId = _id;
             this.speed = 3;
             this.isJump = false;
             this.vx = 0;
             this.vy = 0;
             this.direction = 1;
             this.projectiles = [];
+            this.isDead = false;
+
+            this.onHit = function (_cause) {
+                if (this.isDead) {
+                    return;
+                }
+                console.log('hit');
+                this.isDead = true;
+            }
+
+            this.sync = function(data) {
+                this.x = data.x;
+                this.y = data.y;
+                this.direction = data.direction;
+                this.scaleX = data.direction;
+            }
 
             this.animation = function () {
-
+                if (this.isDead) {
+                    this.frame = 3;
+                    return;
+                }
                 if (this.frame > 1) {
                     this.frame = 0
                 } else {
@@ -69,63 +209,83 @@
                 }
             }
 
-            this.shot = function() {
-                let _projectile = this.projectiles.find(function (p_) { return !p_.visible })
+            this.shot = function () {
+                let _projectile = this.projectiles.find(function (p_) { return !p_.isActive })
                 if (_projectile) {
                     _projectile.launch(this.x + 16, this.y + 16, this.direction);
                 }
             }
 
-            this.move = function (_game) {
-                this.vx = 0;
-                if (_game.input.right) {
-                    this.vx += this.speed;
-                }
-                if (_game.input.left) {
-                    this.vx -= this.speed;
-                }
-
-                if (this.vx) {
-                    if (this.x + this.vx < 0 || this.x + this.vx + 32 > _game.width) {
-                        this.vx = 0
-                    }
-
-                    if ((this.vx > 0 && this.direction < 0) || (this.vx < 0 && this.direction > 0)) {
-                        this.scaleX *= -1;
-                        this.direction *= -1
-                    }
-
-                    this.x += this.vx;
-                }
-
-                if (!this.isJump && _game.input.up) {
-                    this.isJump = true;
-                    this.vy = -10;
-                }
-                if (this.isJump) {
-
-                    if (this.vy && _game.floorHeight <= this.y + this.vy) {
-                        this.y = _game.floorHeight;
-                        this.isJump = false;
-                        // 縦方向リセット
-                        this.vy = 0;
-                    } else {
-                        // 高さを更新
-                        this.y += this.vy;
-                    }
-                    this.vy += 1;
-                }
-
-            }
+            this.addEventListener('enterframe', function () {
+                this.animation();
+            })
         }
     });
 
-    const Projectile = Class.create(Sprite, {
+    const MoveCommand = function (_target, _socket) {
+
+        let self = _target;
+        return function (_game) {
+            self.vx = 0;
+            if (_game.input.right) {
+                self.vx += self.speed;
+            }
+            if (_game.input.left) {
+                self.vx -= self.speed;
+            }
+
+            if (self.vx) {
+                if (self.x + self.vx < 0 || self.x + self.vx + 32 > _game.width) {
+                    self.vx = 0
+                }
+
+                if ((self.vx > 0 && self.direction < 0) || (self.vx < 0 && self.direction > 0)) {
+                    self.scaleX *= -1;
+                    self.direction *= -1
+                }
+
+                self.x += self.vx;
+            }
+
+            if (!self.isJump && _game.input.up) {
+                self.isJump = true;
+                self.vy = -10;
+            }
+
+            if (self.vy && _game.floorHeight <= self.y + self.vy) {
+                self.y = _game.floorHeight;
+                self.isJump = false;
+                // 縦方向リセット
+                self.vy = 0;
+            } else {
+                // 高さを更新
+                self.y += self.vy;
+                self.vy += 1;
+            }
+
+            _socket.emit('sync', {
+                playerId: _target.playerId,
+                x: _target.x,
+                y: _target.y,
+                direction: _target.direction
+            })
+        }
+    }
+
+
+
+    const Projectile = Class.create(Group, {
         initialize: function (_playerId) {
-            Sprite.call(this, 16, 16);
-            this.image = game.assets['./img/icon1.png'];
-            this.visible = false;
+            //Sprite.call(this, 16, 16);
+
+            Group.call(this);
+            let _view = new Sprite(16, 16);
+            _view.image = game.assets['./img/icon1.png'];
+            _view.visible = false;
+
+            this.addChild(_view);
             this.playerId = _playerId;
+            this.isActive = false;
             this.x = 100;
             this.y = 100;
             this.frame = 0;
@@ -133,8 +293,22 @@
             this.ttl = 0;
             this.direction = 1;
 
+            let _collision = new Sprite(16, 16);
+            _collision.backgroundColor = 'blue';
+            _collision.opacity = 0.3;
+            _collision.visible = false;
+            this.addChild(_collision);
+
+
+            this.setActive = function (_value) {
+                this.isActive = _value;
+                _view.visible = _value;
+                // debug
+                _collision.visible = _value;
+            }
+
             this.launch = function (_x, _y, _direction) {
-                if (this.visible) {
+                if (this.isActive) {
                     return;
                 }
 
@@ -142,19 +316,44 @@
                 this.y = _y;
                 this.direction = _direction;
                 this.ttl = 24;
-                this.visible = true;
+                this.setActive(true);
+            }
+
+            this.isIntersect = function (_other) {
+                if (_other.playerId !== this.playerId) {
+                    if (_collision.intersect(_other)) {
+                        return true;
+                    }
+                }
+
+                return false;
             }
 
             this.addEventListener('enterframe', function () {
-                if (this.visible) {
+                if (this.isActive) {
                     this.x += this.speed * this.direction;
+                    for (let _p of players) {
+                        if (this.isIntersect(_p)) {
+                            _collision.backgroundColor = 'red';
+                            if (_p.onHit) {
+                                _p.onHit(this);
+
+                            }
+                        } else {
+                            _collision.backgroundColor = 'blue';
+                        }
+                    }
+
+                    // 生存時間の更新
                     --this.ttl;
                     if (this.ttl < 0) {
-                        this.visible = false;
+                        this.setActive(false);
                     }
                 }
             })
         }
-    })
+    });
+
+
 
 })();
